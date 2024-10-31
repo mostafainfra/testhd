da7b866b0e8f85ff2e13444a5ec444a07cf0de07
mostafa <142887818+mostafainfra@users.noreply.github.com>
2024-10-31 17:47:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,24 @@
-provider "azurerm" {
-  features {}
-}
-
 terraform {
+  backend "azurerm" {
+    resource_group_name   = "rg-shared-mgmt-francecentral-001"
+    storage_account_name  = "stmgmtfcngd001"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
+      version = "~>3.2"
       source  = "hashicorp/azurerm"
-      version = "3.26.0"
     }
   }
 }
 
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "myFirstResourceGroup"
-  location = "westeurope"
+  location = "France Central"
+  name     = "rg-test-github-actions"
 }
